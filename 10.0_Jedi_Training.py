cd3842746f25427eddf3b14021159ae9cd00cc76
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-15 17:57:15 -0500

    Update 10.0_Jedi_Training.py

--- 10.0_Jedi_Training.py
+++ 10.0_Jedi_Training.py
@@ -1,21 +1,22 @@
+# 10.0 Jedi Training (15pts)  Name:________________
+
 '''
-Sign your name:________________
- 
- 1. Write code that defines a class named Animal:
+ANIMAL CLASSES:  (5pts)
+ A. Write code that defines a class named Animal:
      * Add a constructor for the Animal class that prints 'An animal has been born.'
      * Add an attribute for the animal name.
      * Add an eat() method for Animal that prints 'Munch munch.'
      * Add a make_noise() method for Animal that prints 'Grrr says [animal name].'
 
      
- 2. Write code that defines a class named Cat:
+ B. Write code that defines a class named Cat:
      * Make Animal the parent.
      * Add a constructor for Cat that prints 'A cat has been born.'
      * Modify the constructor so it calls the parent constructor as well.
      * Add a make_noise() method for Cat that prints 'Meow says [animal name].'
 
      
- 3. Write code that defines a class named Dog:
+ C. Write code that defines a class named Dog:
      
      * Make Animal the parent.
      * Add a constructor for Dog that prints 'A dog has been born.'
@@ -23,7 +24,7 @@ Sign your name:________________
      * Add a make_noise() method for Dog that prints 'Bark says [animal name].'
 
      
- 4. Write a main program with:
+ D. Write a main program with:
      
      * Code that creates a cat, two dogs, and an animal with names.
      * Code that calls eat() and make_noise() for each animal.
